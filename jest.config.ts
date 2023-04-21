d31335a48483e03d7fc738f0e0ee9e15d19b670a
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-04-21 19:26:04 -0300

    jest config

---
+++ jest.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+const rootDirector = path.resolve(__dirname);
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      function: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  rootDir: rootDirector,
+  roots: [rootDirector],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/build',
+    `${rootDirector}/__tests__/fixtures`,
+    `${rootDirector}/__tests__/setup.ts`,
+    `${rootDirector}/__tests__/mocks/*`,
+  ],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      { tsconfig: path.resolve(__dirname, 'tsconfig.json') },
+    ],
+  },
+  testRegex: ['((/__tests__/.*)|(\\.|/)(test|spec))\\.tsx?$'],
+};
